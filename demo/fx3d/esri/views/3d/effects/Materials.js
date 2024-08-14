4fa32a909f72ed8929a9376d566eb9106cc8910e
Lixian Dai <lixian.dai@aptos.com>
2024-08-14 10:05:26 -0400

    feat: init check in

---
+++ demo/fx3d/esri/views/3d/effects/Materials.js
@@ -0,0 +1,5 @@
+/**
+ * Copyright @ 2024 Esri.
+ * All rights reserved under the copyright laws of the United States and applicable international laws, treaties, and conventions.
+ */
+define(["dojo/_base/declare"],function(e){var t=e(null,{declaredClass:"esri.views.3d.effects.Materials",effectName:"Materials",getOldActiveUnit:function(e){return 0},setup:function(){return this.loadShaders()}});return t});
